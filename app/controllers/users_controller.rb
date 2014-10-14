5c54e966b2dda359fdf62e5a111b9c7f16b06642
Richa Arora <ricarora@gmail.com>
2014-10-14 14:15:55 -0700

    add relations to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def authenticate
+    
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :products, :categories
+  validates :username, :name, presence: true
+  has_secure_password
 end
