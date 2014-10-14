350675384649067fa108b80f6210fee9c69e7648
Richa Arora <ricarora@gmail.com>
2014-10-14 14:39:59 -0700

    add - create, show and destroy for login session

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,7 +1,23 @@
 class LoginsController < ApplicationController
 
+# Create a login, aka "log the user in"
   def create
-    
+    if user = User.authenticate(params[:username], params[:password])
+    # Save the user ID in the session so it can be used in subsequent requests
+      session[:current_user_id] = user.id
+    end
+  end
+
+# returns the user associated with the current session
+  def show
+    @user = User.find(session[:current_user_id])
+  end
+
+# Log off the user
+  def destroy
+    session[:current_user_id] = nil
+    flash[:notice] = "You have successfully logged out."
+    redirect_to root_path
   end
 
 end
