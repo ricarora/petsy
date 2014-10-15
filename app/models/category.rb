9a6034eca35f42f5ef010ff3f16425ab71bd45b6
Kate Fulton <katesuzannefulton@gmail.com>
2014-10-14 21:20:54 -0700

    Add create functionality in categories controller.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :products, through: :product_categories
   has_many :product_categories
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get "/categories", to: "categories#index"
+  get "/categories/new", to: "categories#new"
+  post "/categories/new", to: "categories#create"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
