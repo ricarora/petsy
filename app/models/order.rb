083709907cb800e852f85c898309fa7d369742e6
Kristina Hjertberg <hjert.k@gmail.com>
2014-10-16 09:05:22 -0700

    trying out validation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,18 +2,9 @@ class Order < ActiveRecord::Base
   has_many :orderitems
   has_many :products, through: :orderitems
 
-#   # WHY WON"T THIS WORK?!
-#   include ActiveModel::Validations
-#   validates_with MyValidator
+  ## why won't this work in console?
+  # validates :orderitems, presence: true
 
   #this re-validates orderitems when order is created (verifying that they belong to an order)
   validates_associated :orderitems
 end
-
-# class MyValidator < ActiveModel::Validator
-#   def validate(record)
-#     if record.orderitems.length < 1
-#       record.errors[:orderitems] << "One or more orderitems required"
-#     end
-#   end
-# end
