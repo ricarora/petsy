cc17130cc56f6f45bf2731ff952d7b9966e0b8d9
Kristina Hjertberg <hjert.k@gmail.com>
2014-10-16 15:17:23 -0700

    trying to add a validation for checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,11 @@ class Order < ActiveRecord::Base
 
   #this re-validates orderitems when order is created (verifying that they belong to an order)
   validates_associated :orderitems
+
+  ## trying to validate the credit card form
+  # validates :name_on_card, :card_number, :security_code, :zip, :email, :address, :city, :state, :card_exp, presence: true, unless: :pending?
+
+  def pending?
+    status == "paid"
+  end
 end
