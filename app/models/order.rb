57b8ddd6698b97a02f0aadce019ac5a9b1f47b95
Kristina Hjertberg <hjert.k@gmail.com>
2014-10-16 16:39:56 -0700

    some validations working

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,8 +5,8 @@ class Order < ActiveRecord::Base
   ## why won't this work in console?
   # validates :orderitems, presence: true
 
-  #this re-validates orderitems when order is created (verifying that they belong to an order)
-  # validates_associated :orderitems
+  # this re-validates orderitems when order is created (verifying that they belong to an order)
+  validates_associated :orderitems
 
   ## trying to validate the credit card form
   # validates :name_on_card, :card_number, :security_code, :zip, :email, :address, :city, :state, :card_exp, presence: true, unless: :pending?
