61436fe09485110104e5c87f00be80bcc11a4dba
Kristina Hjertberg <hjert.k@gmail.com>
2014-10-15 18:27:05 -0700

    trying to get validations working

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,19 @@
 class Order < ActiveRecord::Base
   has_many :orderitems
   has_many :products, through: :orderitems
+
+  # WHY WON"T THIS WORK?!
+  include ActiveModel::Validations
+  validates_with MyValidator
+
+  #this will validate orderitems when order is created (and verify that they belong to an order)
+  validates_associated :orderitems
+end
+
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    if record.orderitems.length < 1
+      record.errors[:orderitems] << "One or more orderitems required"
+    end
+  end
 end
--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -2,4 +2,14 @@ class Orderitem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
   belongs_to :cart
+
+  validates :product_id, presence: true
+  validates :qty, presence: true, numericality: { only_integer: true, :greater_than => 0 }
+
+  # this will validate if a cart_id is present (cart_id will be set to nil when added to order)
+  validates :order_id, presence: true, unless: :in_cart?
+
+  def in_cart?
+    cart_id != nil
+  end
 end
