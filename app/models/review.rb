ea829bdf45e5df2b69569f0716dd675469872fd5
Kate Fulton <katesuzannefulton@gmail.com>
2014-10-16 13:57:10 -0700

    Adjust review form format

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,7 @@ class Review < ActiveRecord::Base
   validates :rating, presence: true, numericality: true
   validates_inclusion_of :rating, :in => 1..5
 
+  validates :comment, presence: true
+  
   belongs_to :products
 end
