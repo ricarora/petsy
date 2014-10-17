56899299e5c69e3b6fe59deb789590627edb7a67
Kristina Hjertberg <hjert.k@gmail.com>
2014-10-17 13:37:23 -0700

    tidying shiny new validations

--- app/models/orderitem.rb
+++ app/models/orderitem.rb
@@ -1,10 +1,10 @@
 class Orderitem < ActiveRecord::Base
-  belongs_to :order#, inverse_of: :orderitems
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
 
-  # validates :product_id, presence: true
-  # validates :qty, presence: true, numericality: { only_integer: true, :greater_than => 0 }
+  validates :product_id, presence: true
+  validates :qty, presence: true, numericality: { only_integer: true, :greater_than => 0 }
 
   # this will validate if a cart_id is present and order_id is not (indicating it's in the cart)
   validates :order_id, presence: true, unless: :in_cart?
