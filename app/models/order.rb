484983375801809406ea103189c5b30fe6d59f22
Kristina Hjertberg <hjert.k@gmail.com>
2014-10-17 13:34:54 -0700

    email format validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,13 @@
 class Order < ActiveRecord::Base
-  has_many :orderitems, inverse_of: :order
+  has_many :orderitems#, inverse_of: :order
   has_many :products, through: :orderitems
 
   # validates that orderitems are attached to order (on order.update(status: "paid"))
   validates :orderitems, presence: true, unless: :pending?
 
   # validates the credit card form
-  validates :name_on_card, :card_number, :security_code, :zip, :email, :address, :city, :state, :card_exp, presence: true
+  # validates :name_on_card, :card_number, :security_code, :zip, :email, :address, :city, :state, :card_exp, presence: true
+  # validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "valid email address" }
 
   def pending?
     status == "pending"
