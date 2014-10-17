8479a4b56b1a82940a2c5a5ac90843b60c50c1e8
Lily Pace <lilyfpace@gmail.com>
2014-10-17 16:15:51 -0700

    end of week 1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
 
   def update
     @product = Product.find(params[:id])
-      if @product.update(params.require(:product).permit(:name, :price, :description, :image_url, :stock))
+      if @product.update(params.require(:product).permit(:name, :price, :description, :image_url, :stock, :is_active))
         redirect_to "/products"
       else
         render :edit
@@ -35,7 +35,7 @@ class ProductsController < ApplicationController
     params.inspect
     @product = Product.new(params.require(:product).permit(:name, :price, :description, :image_url, :stock, :is_active))
     if @product.save
-      redirect_to "/products"
+      redirect_to products_index_path
     else
       redirect_to "/products/new"
     end
