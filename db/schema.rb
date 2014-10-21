835333bb01882eb38f6e50829bb830029f1cb4dc
Kristina Hjertberg <hjert.k@gmail.com>
2014-10-21 14:26:42 -0700

    adding user_id column to orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020224226) do
+ActiveRecord::Schema.define(version: 20141021212406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20141020224226) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "card_exp"
+    t.integer  "user_id"
   end
 
   create_table "product_categories", force: true do |t|
