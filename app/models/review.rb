61b141f1bbaa1164eec979c6ab240da44fa2dafd
Kate Fulton <katesuzannefulton@gmail.com>
2014-10-23 09:11:44 -0700

    Remove extra commented out code

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,21 +10,6 @@ class Review < ActiveRecord::Base
 
   belongs_to :product
 end
-#   validate do |review|
-#     UserValidator.new(review).validate
-#   end
-#
-# class UserValidator < ActiveModel::Validator
-#   def initialize(review)
-#    @review = review
-#   end
-#
-#   def validate
-#     if @review.user_id == @review.product.user_id
-#       @review.errors[:user_id] << "You cannot review your own product."
-#     end
-#   end
-# end
 
 class UserValidator < ActiveModel::Validator
   def initialize(review)
